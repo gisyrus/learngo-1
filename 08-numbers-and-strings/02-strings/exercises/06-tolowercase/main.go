eb405f4366c9a8534d8479f45b7c02df6ee90e9d
Syrus Man <syrus.man@agileexlab.net>
2022-07-06 13:39:32 +0800

    Finish: Basic Strings 1-8

--- 08-numbers-and-strings/02-strings/exercises/06-tolowercase/main.go
+++ 08-numbers-and-strings/02-strings/exercises/06-tolowercase/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: ToLowercase
 //
@@ -28,4 +34,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	word := os.Args[1]
+	fmt.Println(strings.ToLower(word))
 }
